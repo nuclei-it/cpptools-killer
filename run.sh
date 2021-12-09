a9128181d7c1b777b307035b125cd25329948bc7
James Swineson <github@public.swineson.me>
2021-12-09 17:23:44 +0800

    convert all tabs to spaces

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ $EUID -ne 0 ]]; then
-   >&2 echo "[-] This script must be run as root"
-   exit 1
+    >&2 echo "[-] This script must be run as root"
+    exit 1
 fi
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
